0bd8898dbe902a4d9927e0bad696dc3c6ba928cb
oksi18 <oksanaklymchuk04@gmail.com>
2022-11-01 14:20:55 +0200

    hm1

---
+++ lesson1js/main.js
@@ -0,0 +1,49 @@
+"use script"
+
+let s = 'hello'
+console.log(s);
+let sm = 'owu'
+console.log(sm)
+let smt = 'com'
+console.log(smt)
+let smth = 'ua'
+console.log(smth)
+
+let number = 1
+console.log(number)
+let number1 = 10
+console.log(number1)
+let number2 = -999
+console.log(number2)
+let number3 = 123
+console.log(number3)
+
+const P = 3.14
+console.log(P)
+
+let number4 = 2.7
+console.log(number4)
+let number5 = 16
+console.log(number5)
+
+let tr = true
+console.log(tr)
+let fl = false
+console.log(fl)
+
+let firstName = 'Оксана'
+console.log(firstName)
+let middleName = 'Ігорівна'
+console.log(middleName)
+let lastName = 'Климчук'
+console.log(lastName)
+console.log(firstName + ' ' + middleName + ' ' + lastName)
+
+let a = 100
+console.log(typeof (a))
+let b = '100'
+console.log(typeof (b))
+let c = true
+console.log(typeof (c))
+
+
