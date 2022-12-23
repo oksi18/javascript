de3806294ac6c113a2e420f6ae3476fb72635e67
oksi18 <oksanaklymchuk04@gmail.com>
2022-12-23 21:55:13 +0200

    not all project

--- mini project/post-details.js
+++ mini project/post-details.js
@@ -4,7 +4,7 @@ let id = urldetails.searchParams.get('id')
 fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
   .then(value => value.json())
   .then(posts => {
-    for (const post in posts) {
+    for (const post of posts) {
       let divdetails = document.createElement('div')
       let p = document.createElement('p')
       p.innerText =   `${post.userId} ${post.id} ${post.title} ${post.body}`
@@ -12,8 +12,20 @@ fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
       document.body.appendChild(divdetails)
 
 
+      fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+        .then(value => value)
+        .then(comments => {
+          for (const comment of comments) {
+            let div = document.createElement('div')
+            let p = document.createElement('p')
+            p.innerText = `${comment.postId} ${comment.id} ${comment.name} ${comment.email} ${comment.body}`
+            div.appendChild(p)
+            document.body.appendChild(div)
+          }
+        })
 
 
 
     }
   })
+
