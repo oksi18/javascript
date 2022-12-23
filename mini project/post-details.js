7ae5780a596eacd704c3750850b9590f84b9e845
oksi18 <oksanaklymchuk04@gmail.com>
2022-12-23 22:15:04 +0200

    not all project

--- mini project/post-details.js
+++ mini project/post-details.js
@@ -7,25 +7,26 @@ fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
     for (const post of posts) {
       let divdetails = document.createElement('div')
       let p = document.createElement('p')
-      p.innerText =   `${post.userId} ${post.id} ${post.title} ${post.body}`
+      p.innerText =   `UserId: ${post.userId} id: ${post.id} title: ${post.title} body: ${post.body}`
       divdetails.appendChild(p)
       document.body.appendChild(divdetails)
 
 
-      fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-        .then(value => value)
-        .then(comments => {
-          for (const comment of comments) {
-            let div = document.createElement('div')
-            let p = document.createElement('p')
-            p.innerText = `${comment.postId} ${comment.id} ${comment.name} ${comment.email} ${comment.body}`
-            div.appendChild(p)
-            document.body.appendChild(div)
-          }
-        })
+
+
 
 
 
     }
   })
-
+fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+  .then(value => value.json())
+  .then(comments => {
+    for (const comment of comments) {
+      let div = document.createElement('div')
+      let p = document.createElement('p')
+      p.innerText = `postId: ${comment.postId} id: ${comment.id} name: ${comment.name} email: ${comment.email} body: ${comment.body}`
+      div.appendChild(p)
+      document.body.appendChild(div)
+    }
+  })
